858a13f63626d45af3a556aead1ff70431e7d27a
Robert Eisele <robert@xarg.org>
2026-09-08 10:02:13 +0200

    Added dist file

--- dist/unionfind.js
+++ dist/unionfind.js
@@ -41,10 +41,10 @@ __export(unionfind_exports, {
 });
 module.exports = __toCommonJS(unionfind_exports);
 /**
- * @license UnionFind.js v0.1.0
+ * @license UnionFind.js v0.1.1
  * https://github.com/rawify/UnionFind.js
  *
- * Copyright (c) 2025, Robert Eisele (https://raw.org/)
+ * Copyright (c) 2026, Robert Eisele (https://raw.org/)
  * Licensed under the MIT license.
  */
 var _parentsOrSize, _componentCount, _UnionFind_instances, isValidIndex_fn, requireRoot_fn;
--- dist/unionfind.mjs
+++ dist/unionfind.mjs
@@ -17,10 +17,10 @@ var __privateWrapper = (obj, member, setter, getter) => ({
 
 // src/unionfind.ts
 /**
- * @license UnionFind.js v0.1.0
+ * @license UnionFind.js v0.1.1
  * https://github.com/rawify/UnionFind.js
  *
- * Copyright (c) 2025, Robert Eisele (https://raw.org/)
+ * Copyright (c) 2026, Robert Eisele (https://raw.org/)
  * Licensed under the MIT license.
  */
 var _parentsOrSize, _componentCount, _UnionFind_instances, isValidIndex_fn, requireRoot_fn;
